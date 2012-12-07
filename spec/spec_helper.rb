48eb04ed3952672c38515c46ced1feaa8d6359c8
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-12-07 15:10:03 +0000

    Add SimpleCov config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,14 @@
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'govuk_content_models/test_helpers/factories'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
