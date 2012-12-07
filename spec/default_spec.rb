9e5a754a8ce2bafcc4f6786359af7ec6eba25ede
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-12-07 15:16:02 +0000

    Remove redundant spec

--- spec/default_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-# TODO: This spec can be removed once meaningful specs exist. 
-#
-describe "Travel advice publisher application" do
-  it "should make an assertion for ci purposes" do
-    true.should == true
-  end
-end
