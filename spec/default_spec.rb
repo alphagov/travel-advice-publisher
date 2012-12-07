78afb70115f675efa3e372d0e50a5c252ec8b840
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-12-07 11:42:56 +0000

    Add an arbitrary assertion for ci

---
+++ spec/default_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+# TODO: This spec can be removed once meaningful specs exist. 
+#
+describe "Travel advice publisher application" do
+  it "should make an assertion for ci purposes" do
+    true.should == true
+  end
+end
