542279d03d8285888c567729241b1614dd6e5a09
Jordan Hatch <jordan@jordanh.net>
2012-12-20 14:17:27 +0000

    Remove default controller

--- app/controllers/admin/default_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::DefaultController < ApplicationController
-  
-  include Admin::AdminControllerMixin
-
-  def index
-    render :text => "Test output. Remove this once real specs exist."
-  end
-
-end
