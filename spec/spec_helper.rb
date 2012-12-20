cbcfcc6ca8a3a31a44e8108a689d8bcede1c7e90
Jordan Hatch <jordan@jordanh.net>
2012-12-20 14:18:24 +0000

    Add warden stubbing to spec setup
    Also add 'format=d' to rspec arguments, as the test
    output is a bit prettier.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,3 +42,19 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 end
+
+def login_as(user)
+  request.env['warden'] = stub(
+    :authenticate! => true,
+    :authenticated? => true,
+    :user => user
+  )
+end
+
+def stub_user
+  FactoryGirl.create(:user)
+end
+
+def login_as_stub_user
+  login_as stub_user
+end
