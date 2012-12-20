114f6910dc27ce5ac84644fa030f63c8febe3699
Jordan Hatch <jordan@jordanh.net>
2012-12-20 14:54:06 +0000

    Add show method to CountriesController

--- app/controllers/admin/countries_controller.rb
+++ app/controllers/admin/countries_controller.rb
@@ -4,4 +4,8 @@ class Admin::CountriesController < ApplicationController
   def index
     @countries = Country.all
   end
+
+  def show
+    @country = Country.find_by_slug(params[:id]) || error_404
+  end
 end
--- spec/controllers/admin/countries_controller_spec.rb
+++ spec/controllers/admin/countries_controller_spec.rb
@@ -11,13 +11,40 @@ describe Admin::CountriesController do
   describe "GET index" do
     it "populates an array of countries" do
       get :index
+
       assigns(:countries).map(&:slug).should include('afghanistan','albania','algeria')
       assigns(:countries).map(&:name).should include('Afghanistan','Albania','Algeria')
     end
 
     it "renders the index view" do
       get :index
+
       response.should render_template :index
+    end
+  end
+
+  describe "GET show" do
+    describe "given a valid country" do
+      it "assigns the request country" do
+        get :show, id: "australia"
+
+        assigns(:country).name.should eq('Australia')
+        assigns(:country).slug.should eq('australia')
+      end
+
+      it "renders the show view" do
+        get :show, id: "australia"
+
+        response.should render_template :show
+      end
+    end
+
+    describe "given an invalid country" do
+      it "returns a 404" do
+        get :show, id: "middle-earth"
+
+        response.should be_not_found
+      end
     end
   end
 
