20ed72a0e9e139afd39046e977ed583c09474236
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-01-04 15:25:42 +0000

    Add authentication helpers for feature specs.

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,4 +1,4 @@
-module AuthenticationTestHelpers
+module AuthenticationControllerHelpers
   def login_as(user)
     request.env['warden'] = stub(
       :authenticate! => true,
@@ -15,5 +15,19 @@ module AuthenticationTestHelpers
     login_as stub_user
   end
 end
+RSpec.configuration.include AuthenticationControllerHelpers, :type => :controller
 
-RSpec.configuration.include AuthenticationTestHelpers, :type => :controller
+module AuthenticationFeatureHelpers
+  def login_as(user)
+    GDS::SSO.test_user = user
+  end
+
+  def stub_user
+    FactoryGirl.create(:user)
+  end
+
+  def login_as_stub_user
+    login_as stub_user
+  end
+end
+RSpec.configuration.include AuthenticationFeatureHelpers, :type => :feature
