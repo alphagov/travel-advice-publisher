e2752d4b5cfb857fb13493539f730f3a8bdd4b4a
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-01-07 10:35:47 +0000

    Add updated date and edit links to version index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def edition_edlt_link(edition)
+    link_to (edition.draft? ? 'edit' : 'view details'), '#'
+  end
+
+  def timestamp(time)
+    %{<time datetime="#{ time.strftime("%Y-%m-%dT%H:%M:%SZ") }">#{ time.strftime("%d/%m/%Y %H:%M") }</time>}.html_safe
+  end
 end
--- spec/features/country_show_page_spec.rb
+++ spec/features/country_show_page_spec.rb
@@ -7,23 +7,22 @@ feature "Country show page" do
   end
 
   specify "seeing a list of editions for a country" do
-    FactoryGirl.create(:travel_advice_edition, :country_slug => "aruba", :state => "archived", :version_number => 1)
-    FactoryGirl.create(:travel_advice_edition, :country_slug => "aruba", :state => "archived", :version_number => 2)
-    FactoryGirl.create(:travel_advice_edition, :country_slug => "aruba", :state => "published", :version_number => 3)
-    FactoryGirl.create(:travel_advice_edition, :country_slug => "aruba", :state => "draft", :version_number => 4)
+    e1 = FactoryGirl.create(:travel_advice_edition, :country_slug => "aruba", :state => "archived", :version_number => 1)
+    e2 = FactoryGirl.create(:travel_advice_edition, :country_slug => "aruba", :state => "archived", :version_number => 2)
+    e3 = FactoryGirl.create(:travel_advice_edition, :country_slug => "aruba", :state => "published", :version_number => 3)
+    e4 = FactoryGirl.create(:travel_advice_edition, :country_slug => "aruba", :state => "draft", :version_number => 4)
 
     visit "/admin/countries/aruba"
 
     page.should have_content("Aruba")
 
-    table = page.find('table')
-    rows = table.all('tr').map {|r| r.all('th, td').map(&:text).map(&:strip) }
+    rows = page.all('table tr').map {|r| r.all('th, td').map(&:text).map(&:strip) }
     rows.should == [
-      ["Version", "State"],
-      ["Version 4", "draft"],
-      ["Version 3", "published"],
-      ["Version 2", "archived"],
-      ["Version 1", "archived"],
+      ["Version", "State", "Updated", ""],
+      ["Version 4", "draft", e4.updated_at.strftime("%d/%m/%Y %H:%M"), "edit"],
+      ["Version 3", "published", e3.updated_at.strftime("%d/%m/%Y %H:%M"), "view details"],
+      ["Version 2", "archived", e2.updated_at.strftime("%d/%m/%Y %H:%M"), "view details"],
+      ["Version 1", "archived", e1.updated_at.strftime("%d/%m/%Y %H:%M"), "view details"],
     ]
   end
 
