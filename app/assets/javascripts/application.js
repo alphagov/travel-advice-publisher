fbf81e601842565eca06ad3867d770e496076769
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-01-09 17:54:40 +0000

    Add edit edition feature

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,92 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui.custom.min
+//= require jquery.mustache
+//= require twitter/bootstrap
+//= require_tree .
+
+var form_ids = {};
+
+$(function () {
+  $('.add-associated').click(function () {
+    elem = $(this);
+    var target_id = elem.data('target');
+    var target = $('#' + target_id);
+
+    var template_id = elem.data('tmpl_id');
+    template_contents = $('#' + template_id).html();
+
+    if (typeof(form_ids[template_id]) == 'undefined') {
+      var current_id = target.find('fieldset input').last().attr('id').match(/_attributes_(\d+)_/)[1];
+      form_ids[template_id] = current_id;
+    }
+
+    form_ids[template_id]++;
+
+    var html = $.mustache(template_contents, {
+      index: form_ids[template_id]
+    });
+
+    target.append(html);
+    $(this).trigger('associated-added');
+    return false;
+  });
+
+  $('.remove-associated').live('click', function () {
+    var css_selector = $(this).data('selector');
+    $(this).parents(css_selector).hide();
+    $(this).prev(':input').val('1');
+    $('body').trigger('associated-removed');
+    return false;
+  })
+});
+
+// Javascript specific to travel advice admin
+$(function() {
+  // collapse the parts using the bootstrap accordion
+  $(".collapse").collapse();
+
+  var sortable_opts = {
+    axis: "y",
+    handle: "a.accordion-toggle",
+    stop: function(event, ui) {
+      $('.part').each(function (i, elem) {
+        $(elem).find('input.order').val(i + 1);
+        ui.item.find("a.accordion-toggle").addClass("highlight");
+        setTimeout(function() { $("a.accordion-toggle.highlight").removeClass("highlight") }, 20 )
+      });
+    }
+  }
+  //$('#parts').sortable(sortable_opts)
+  //    .find("a.accordion-toggle").css({cursor: 'move'});
+
+  // simulate a click on the first part to open it
+  $('#parts .part .accordion-body').first().collapse('show');
+
+  $('input.title').
+    live('change', function () {
+      var elem = $(this);
+      var value = elem.val();
+
+      // Set slug on change.
+      var slug_field = elem.closest('.part').find('.slug');
+      if (slug_field.text() === '') {
+        // slug_field.val(GovUKGuideUtils.convertToSlug(value));
+        slug_field.val(value);
+      }
+
+      // Set header on change.
+      var header = elem.closest('fieldset').prev('h3').find('a');
+      header.text(value);
+    });
+
+  $('.add-associated').bind('associated-added', function () {
+    var active_index = $('#parts div.part').length;
+    var new_part = $('#parts .part:last-child');
+    new_part.find('.collapse').attr('id', 'new-part-' + active_index).collapse('show');
+    new_part.find('a.accordion-toggle').attr('href', '#new-part-' + active_index);
+
+    new_part.find('input.order').val(active_index);
+    new_part.find('.title').focus();
+  });
+});
---
+++ spec/features/edition_edit_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+feature "Edit Edition page" do
+  before :each do
+    Capybara.current_driver = Capybara.javascript_driver 
+    login_as_stub_user
+    @countries = YAML.load_file(Rails.root.join('spec/fixtures/data/countries.yml'))
+    @edition = FactoryGirl.create(:travel_advice_edition, :country_slug => 'albania', :state => 'draft')
+  end
+
+  scenario "adding parts in the edition form" do
+    visit "/admin/editions/#{@edition._id}/edit"
+    within(:css, '.container-fluid[role=main]') do
+      page.should have_content "Editing Albania"
+    end
+    
+    within(:css, '.row-fluid .span8') do
+      page.should have_content "Parts"
+      page.should have_button "Add new part"
+    end
+
+    click_on 'Untitled part'
+    within :css, '#parts div.part:first-of-type' do
+      fill_in 'Title', :with => 'Part One'
+      fill_in 'Body',  :with => 'Body text'
+      fill_in 'Slug',  :with => 'part-one'
+    end
+
+    click_on 'Add new part'
+    within :css, '#parts div.part:nth-of-type(2)' do
+      fill_in 'Title', :with => 'Part Two'
+      fill_in 'Body',  :with => 'Body text'
+      fill_in 'Slug',  :with => 'part-two'
+    end
+
+    within(:css, '.workflow_buttons') { click_on 'Save' }
+
+    assert_equal 2, all(:css, '#parts > div.part').length
+
+    assert page.has_content? 'Editing Albania'
+  end
+
+end
