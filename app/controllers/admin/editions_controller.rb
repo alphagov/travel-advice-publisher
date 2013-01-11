ec72d51f432f08d3d161f157fe3e262b377d0a1b
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-01-11 17:01:54 +0000

    Add before_filter for country and edition lookup

--- app/controllers/admin/editions_controller.rb
+++ app/controllers/admin/editions_controller.rb
@@ -1,6 +1,7 @@
 class Admin::EditionsController < ApplicationController
 
   before_filter :load_country, :only => [:create]
+  before_filter :load_country_and_edition, :only => [:edit, :update]
 
   def create
     @edition = @country.build_new_edition
@@ -9,11 +10,6 @@ class Admin::EditionsController < ApplicationController
     else
       redirect_to admin_country_path(@country.slug), :alert => "Failed to create new edition"
     end
-  end
-
-  def edit
-    @edition = TravelAdviceEdition.find(params[:id])
-    @country = Country.find_by_slug(@edition.country_slug)
   end
 
   def update
@@ -28,6 +24,11 @@ class Admin::EditionsController < ApplicationController
   end
 
   private
+
+  def load_country_and_edition
+    @edition = TravelAdviceEdition.find(params[:id])
+    @country = Country.find_by_slug(@edition.country_slug)
+  end
 
   def load_country
     @country = Country.find_by_slug(params[:country_id])
