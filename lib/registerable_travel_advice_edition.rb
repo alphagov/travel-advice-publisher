c9f8b0faf74236a6486af45566c6196f8aee95b4
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-01-18 16:52:59 +0000

    Add RegisterableTravelAdviceEdition

---
+++ lib/registerable_travel_advice_edition.rb
@@ -0,0 +1,25 @@
+class RegisterableTravelAdviceEdition
+  extend Forwardable
+
+  def_delegators :@edition, :title, :indexable_content
+
+  def initialize(edition)
+    @edition = edition
+  end
+
+  def state
+    case @edition.state
+    when 'published' then 'live'
+    when 'archived' then 'archived'
+    else 'draft'
+    end
+  end
+
+  def description
+    @edition.overview
+  end
+
+  def slug
+    "travel-advice/#{@edition.country_slug}"
+  end
+end
---
+++ spec/lib/registerable_travel_advice_edition_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe RegisterableTravelAdviceEdition do
+
+  describe "state" do
+    before :each do
+      @edition = FactoryGirl.build(:travel_advice_edition)
+    end
+
+    it "should be 'live' for a published edition" do
+      @edition.state = 'published'
+      RegisterableTravelAdviceEdition.new(@edition).state.should == 'live'
+    end
+
+    it "should be 'archived' for an archived edition" do
+      @edition.state = 'archived'
+      RegisterableTravelAdviceEdition.new(@edition).state.should == 'archived'
+    end
+
+    it "should be 'draft' for a draft edition" do
+      @edition.state = 'draft'
+      RegisterableTravelAdviceEdition.new(@edition).state.should == 'draft'
+    end
+  end
+
+  describe "passed-through fields" do
+    before :each do
+      @edition = FactoryGirl.build(:travel_advice_edition)
+      @registerable = RegisterableTravelAdviceEdition.new(@edition)
+    end
+
+    it "should return the edition's overview for description" do
+      @edition.overview = 'fooey gooey kablooie'
+      @registerable.description.should == 'fooey gooey kablooie'
+    end
+
+    it "should return the edition's country_slug with travel_advice prepended" do
+      @registerable.slug.should == "travel-advice/#{@edition.country_slug}"
+    end
+
+    it "should return the edition's title" do
+      @edition.title = "Aruba travel advice"
+      @registerable.title.should == "Aruba travel advice"
+    end
+
+    it "should return the edition's indexable_content" do
+      @edition.parts << Part.new(:title => "Foo", :body => "Bar")
+      @registerable.indexable_content.should == @edition.indexable_content
+    end
+  end
+end
