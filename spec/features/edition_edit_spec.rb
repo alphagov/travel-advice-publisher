6f50071719f85795ee156ae3c6823bfd902e8399
Kushal Pisavadia <kushi.p@gmail.com>
2013-01-22 15:29:40 +0000

    Use rspec-style assertions

--- spec/features/edition_edit_spec.rb
+++ spec/features/edition_edit_spec.rb
@@ -11,7 +11,7 @@ feature "Edit Edition page", :js => true do
 
     click_on "Create new edition"
 
-    assert page.has_field?("Title", :with => "Aruba travel advice")
+    page.should have_field("Title", :with => "Aruba travel advice")
     page.should have_content("Untitled part")
   end
 
@@ -21,7 +21,7 @@ feature "Edit Edition page", :js => true do
     within(:css, '.container-fluid[role=main]') do
       page.should have_content "Editing Albania"
     end
-    
+
     within(:css, '.row-fluid .span8') do
       page.should have_content "Parts"
       page.should have_button "Add new part"
@@ -43,21 +43,21 @@ feature "Edit Edition page", :js => true do
 
     within(:css, '.workflow_buttons') { click_on 'Save' }
 
-    assert_equal 2, all(:css, '#parts > div.part').length
-    
+    all(:css, '#parts > div.part').length.should == 2
+
     current_path.should == "/admin/editions/#{@edition._id}/edit"
   end
 
   scenario "slug for parts should be automatically generated" do
     @edition = FactoryGirl.create(:travel_advice_edition, :country_slug => 'albania', :state => 'draft')
-
     visit "/admin/editions/#{@edition._id}/edit"
 
     click_on 'Untitled part'
     within :css, '#parts div.part:first-of-type' do
       fill_in 'Title', :with => 'Part One'
       fill_in 'Body',  :with => 'Body text'
-      assert_equal 'part-one', find(:css, ".slug").value
+
+      find(:css, ".slug").value.should == 'part-one'
     end
   end
 
@@ -67,14 +67,22 @@ feature "Edit Edition page", :js => true do
     @edition.parts.build(:title => 'Part Two', :body => 'Body text', :slug => 'part-two')
     @edition.save!
 
+    @edition.parts.build
+    p1 = @edition.parts.first.update_attributes(
+      :title => 'Part One', :body => 'Body text', :slug => 'part-one')
+
+    @edition.parts.build
+    p2 = @edition.parts.second.update_attributes(
+      :title => 'Part Two', :body => 'Body text', :slug => 'part-two')
+
     visit "/admin/editions/#{@edition._id}/edit"
-    
+
     click_on 'Part Two'
 
     within :css, '#parts div.part:nth-of-type(2)' do
       click_on 'Remove part'
     end
-    
+
     page.should have_css('#part-two', :visible => false)
 
     # page.execute_script("$('.remove-associated').last().prev(':input').val('1')")
