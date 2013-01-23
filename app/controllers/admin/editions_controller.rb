0d12bd71e7caac08f7fc08eee06b55515b84c183
Kushal Pisavadia <kushi.p@gmail.com>
2013-01-23 16:45:24 +0000

    Use the publish_as method to track which user published the edition

--- app/controllers/admin/editions_controller.rb
+++ app/controllers/admin/editions_controller.rb
@@ -27,7 +27,7 @@ class Admin::EditionsController < ApplicationController
 
   def publish
     @edition = TravelAdviceEdition.find(params[:id])
-    if @edition.publish
+    if @edition.publish_as(current_user)
       redirect_to admin_country_path(@edition.country_slug), :alert => "#{@edition.title} published."
     else
       redirect_to edit_admin_edition_path(@edition), :alert => "Only draft editions can be published."
