4623fa840c1e7384b397b82cdc0fafc9caa21d04
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-02-14 15:20:40 +0000

    Prevent us being live in production yet
    This allows us to set a constant in deploy scripts so that only preview
    registers artefacts as live

--- lib/registerable_travel_advice_edition.rb
+++ lib/registerable_travel_advice_edition.rb
@@ -9,7 +9,7 @@ class RegisterableTravelAdviceEdition
 
   def state
     case @edition.state
-    when 'published' then 'live'
+    when 'published' then self.class.globally_live? ? 'live' : 'draft'
     when 'archived' then 'archived'
     else 'draft'
     end
@@ -21,5 +21,15 @@ class RegisterableTravelAdviceEdition
 
   def slug
     "foreign-travel-advice/#{@edition.country_slug}"
+  end
+
+  # Temporary thing to prevent things appearing in search etc. in production until we're really live.
+  # TRAVEL_ADVICE_LIVE will be set in an initializer for preview
+  def self.globally_live?
+    if Rails.env.production?
+      defined?(TRAVEL_ADVICE_LIVE) && TRAVEL_ADVICE_LIVE
+    else
+      true
+    end
   end
 end
