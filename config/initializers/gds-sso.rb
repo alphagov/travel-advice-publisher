46bb437f5ae57283f819328881c0e52472d50ae9
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-06-07 10:37:14 +0100

    Fix dev sso config

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,9 +1,6 @@
 GDS::SSO.config do |config|
   config.user_model   = 'User'
-  config.oauth_id     = ENV['OAUTH_ID']
-  config.oauth_secret = ENV['OAUTH_SECRET']
-  config.default_scope = "Travel Advice Publisher"
+  config.oauth_id     = 'abcdefghjasndjkasndtraveladvicepublisher'
+  config.oauth_secret = 'secret'
   config.oauth_root_url = Plek.current.find("signon")
-  config.basic_auth_user = 'api'
-  config.basic_auth_password = 'secret'
 end
