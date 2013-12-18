9fa58cbe757d38a37fd14248247c865a3274e822
Vinay Patel <mail@vinayvinay.com>
2013-12-18 17:44:42 +0000

    Enabling Logstasher in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ TravelAdvicePublisher::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :ses
-  
+
   # Enable threaded mode
   # config.threadsafe!
 
@@ -62,4 +62,8 @@ TravelAdvicePublisher::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  # Enable JSON-style logging
+  config.logstasher.enabled = true
+  config.logstasher.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.json.log")
+  config.logstasher.supress_app_log = true
 end
