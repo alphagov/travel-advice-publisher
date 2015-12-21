a076faa1193258b0054360c924b7e4978ba3a9ee
Chris Patuzzo <chris@patuzzo.co.uk>
2015-12-21 14:00:22 +0000

    Migrate republishing rake task to publishing api /v2

--- lib/tasks/publishing_api.rake
+++ lib/tasks/publishing_api.rake
@@ -1,17 +1,23 @@
-
 namespace :publishing_api do
+  def api_v2
+    TravelAdvicePublisher.publishing_api_v2
+  end
 
   desc "send index content-item to publishing-api"
   task :publish => :environment do
     presenter = IndexPresenter.new
-    TravelAdvicePublisher.publishing_api.put_content_item(presenter.base_path, presenter.render_for_publishing_api)
+
+    api_v2.put_content(presenter.content_id, presenter.render_for_publishing_api)
+    api_v2.publish(presenter.content_id, presenter.update_type)
   end
 
   desc "republish all published editions to publishing-api"
   task :republish_editions => :environment do
     TravelAdviceEdition.published.each do |edition|
       presenter = EditionPresenter.new(edition, republish: true)
-      TravelAdvicePublisher.publishing_api.put_content_item(presenter.base_path, presenter.render_for_publishing_api)
+
+      api_v2.put_content(presenter.content_id, presenter.render_for_publishing_api)
+      api_v2.publish(presenter.content_id, presenter.update_type)
     end
   end
 end
--- spec/tasks/publishing_api_rake_spec.rb
+++ spec/tasks/publishing_api_rake_spec.rb
@@ -2,25 +2,30 @@ require 'spec_helper'
 require 'rake'
 
 describe "publishing_api take tasks", :type => :rake_task do
-  include GdsApi::TestHelpers::PublishingApi
+  include GdsApi::TestHelpers::PublishingApiV2
 
-  before :each do
+  before do
     Rake.application = nil # Reset any previously loaded tasks
     Rails.application.load_tasks
+
+   stub_any_publishing_api_call
   end
 
   describe "publish" do
     let(:task) { Rake::Task['publishing_api:publish'] }
 
     it "sends the index item to publishing_api" do
-      stub_default_publishing_api_put
-
       task.invoke
 
-      assert_publishing_api_put_item("/foreign-travel-advice", {
-        "title" => "Foreign travel advice",
-        "format" => "placeholder_travel_advice_index",
-        'content_id' => TravelAdvicePublisher::INDEX_CONTENT_ID,
+      assert_publishing_api_put_content(TravelAdvicePublisher::INDEX_CONTENT_ID, {
+        base_path: "/foreign-travel-advice",
+        title: "Foreign travel advice",
+        format: "placeholder_travel_advice_index",
+        update_type: "minor",
+      })
+
+      assert_publishing_api_publish(TravelAdvicePublisher::INDEX_CONTENT_ID, {
+        update_type: "minor",
       })
     end
   end
@@ -28,28 +33,34 @@ describe "publishing_api take tasks", :type => :rake_task do
   describe "republish_editions" do
     let(:task) { Rake::Task['publishing_api:republish_editions'] }
 
-    before :each do
-      stub_default_publishing_api_put
-    end
-
     it "sends all published editions to the publishing_api with update_type of 'republish'" do
       aruba = FactoryGirl.create(:published_travel_advice_edition, :country_slug => 'aruba', :published_at => 10.minutes.ago)
       algeria = FactoryGirl.create(:published_travel_advice_edition, :country_slug => 'algeria', :published_at => 5.minutes.ago)
 
       task.invoke
 
-      assert_publishing_api_put_item("/foreign-travel-advice/aruba", {
-        "title" => aruba.title,
-        "format" => "placeholder_travel_advice",
-        "update_type" => "republish",
-        "public_updated_at" => aruba.published_at.iso8601,
+      assert_publishing_api_put_content("56bae85b-a57c-4ca2-9dbd-68361a086bb3", {
+        base_path: "/foreign-travel-advice/aruba",
+        title: aruba.title,
+        format: "placeholder_travel_advice",
+        update_type: "republish",
+        public_updated_at: aruba.published_at.iso8601,
       })
 
-      assert_publishing_api_put_item("/foreign-travel-advice/algeria", {
-        "title" => algeria.title,
-        "format" => "placeholder_travel_advice",
-        "update_type" => "republish",
-        "public_updated_at" => algeria.published_at.iso8601,
+      assert_publishing_api_publish("56bae85b-a57c-4ca2-9dbd-68361a086bb3", {
+        update_type: "republish",
+      })
+
+      assert_publishing_api_put_content("b5c8e64b-3461-4447-9144-1588e4a84fe6", {
+        base_path: "/foreign-travel-advice/algeria",
+        title: algeria.title,
+        format: "placeholder_travel_advice",
+        update_type: "republish",
+        public_updated_at: algeria.published_at.iso8601,
+      })
+
+      assert_publishing_api_publish("b5c8e64b-3461-4447-9144-1588e4a84fe6", {
+        update_type: "republish",
       })
     end
 
@@ -59,10 +70,14 @@ describe "publishing_api take tasks", :type => :rake_task do
 
       task.invoke
 
-      expect(a_request(:put, GdsApi::TestHelpers::PublishingApi::PUBLISHING_API_ENDPOINT + "/content/foreign-travel-advice/aruba"))
+      expect(a_request(:put, GdsApi::TestHelpers::PublishingApi::PUBLISHING_API_ENDPOINT + "/v2/content/56bae85b-a57c-4ca2-9dbd-68361a086bb3"))
         .not_to have_been_made
 
-      assert_publishing_api_put_item("/foreign-travel-advice/algeria")
+      expect(a_request(:post, GdsApi::TestHelpers::PublishingApi::PUBLISHING_API_ENDPOINT + "/v2/content/56bae85b-a57c-4ca2-9dbd-68361a086bb3/publish"))
+        .not_to have_been_made
+
+      assert_publishing_api_put_content("b5c8e64b-3461-4447-9144-1588e4a84fe6")
+      assert_publishing_api_publish("b5c8e64b-3461-4447-9144-1588e4a84fe6")
     end
   end
 end
