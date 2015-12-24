1a496be99e1278b66cc42238a61ada90cf6287e7
Chris Patuzzo <chris@patuzzo.co.uk>
2015-12-24 09:57:54 +0000

    Check the entire index content item rather than a subset of attrs

--- app/presenters/index_presenter.rb
+++ app/presenters/index_presenter.rb
@@ -10,29 +10,20 @@ class IndexPresenter
   def render_for_publishing_api
     {
       "content_id" => content_id,
-      "base_path" => base_path,
+      "base_path" => "/foreign-travel-advice",
       "format" => "placeholder_travel_advice_index",
       "title" => "Foreign travel advice",
       "description" => "Latest travel advice by country including safety and security, entry requirements, travel warnings and health",
       "locale" => "en",
       "publishing_app" => "travel-advice-publisher",
       "rendering_app" => "frontend",
-      "routes" => routes,
+      "routes" => [
+        {"path" => "/foreign-travel-advice", "type" => "exact"},
+        {"path" => "/foreign-travel-advice.atom", "type" => "exact"},
+        {"path" => "/foreign-travel-advice.json", "type" => "exact"},
+      ],
       "public_updated_at" => Time.zone.now.iso8601,
       "update_type" => update_type,
     }
-  end
-
-private
-  def base_path
-    "/foreign-travel-advice"
-  end
-
-  def routes
-    [
-      {"path" => base_path, "type" => "exact"},
-      {"path" => "#{base_path}.atom", "type" => "exact"},
-      {"path" => "#{base_path}.json", "type" => "exact"},
-    ]
   end
 end
--- spec/presenters/index_presenter_spec.rb
+++ spec/presenters/index_presenter_spec.rb
@@ -1,45 +1,45 @@
 require 'spec_helper'
 
 describe IndexPresenter do
-  let(:presenter) { IndexPresenter.new }
-
-  it "returns the index content_id" do
-    expect(presenter.content_id).to eq(TravelAdvicePublisher::INDEX_CONTENT_ID)
+  describe "#content_id" do
+    it "returns the index content_id" do
+      expect(subject.content_id).to eq(TravelAdvicePublisher::INDEX_CONTENT_ID)
+    end
   end
 
-  describe "render_for_publishing_api" do
-    let(:presented_data) { presenter.render_for_publishing_api }
-
-    it "returns a placeholder item" do
-
-      expect(presented_data).to include({
-        "format" => "placeholder_travel_advice_index",
-        "title" => "Foreign travel advice",
-        "description" => "Latest travel advice by country including safety and security, entry requirements, travel warnings and health",
-        "content_id" => TravelAdvicePublisher::INDEX_CONTENT_ID,
-        "locale" => "en",
-        "publishing_app" => "travel-advice-publisher",
-        "rendering_app" => "frontend",
-        "update_type" => "minor",
-      })
+  describe "#update_type" do
+    it "returns the update_type of the edition" do
+      expect(subject.update_type).to eq("minor")
     end
+  end
 
-    it "sets public_updated_at to now" do
-      Timecop.freeze do
-        expect(presented_data["public_updated_at"]).to eq(Time.zone.now.iso8601)
-      end
-    end
-
-    it "creates the necessary routes for the index" do
-      expect(presented_data["routes"]).to match_array([
-        {"path" => "/foreign-travel-advice", "type" => "exact"},
-        {"path" => "/foreign-travel-advice.atom", "type" => "exact"},
-        {"path" => "/foreign-travel-advice.json", "type" => "exact"},
-      ])
-    end
+  describe "#render_for_publishing_api" do
+    let(:presented_data) { subject.render_for_publishing_api }
 
     it "is valid against the content schemas", :schema_test => true do
       expect(presented_data).to be_valid_against_schema('placeholder')
+    end
+
+    it "returns a placeholder item" do
+      Timecop.freeze do
+        expect(presented_data).to eq(
+          "content_id" => TravelAdvicePublisher::INDEX_CONTENT_ID,
+          "base_path" => "/foreign-travel-advice",
+          "format" => "placeholder_travel_advice_index",
+          "title" => "Foreign travel advice",
+          "description" => "Latest travel advice by country including safety and security, entry requirements, travel warnings and health",
+          "locale" => "en",
+          "publishing_app" => "travel-advice-publisher",
+          "rendering_app" => "frontend",
+          "routes" => [
+            { "path"=>"/foreign-travel-advice", "type" => "exact" },
+            { "path" => "/foreign-travel-advice.atom", "type" => "exact" },
+            { "path" => "/foreign-travel-advice.json", "type" => "exact" },
+          ],
+          "public_updated_at" => Time.zone.now.iso8601,
+          "update_type" => "minor",
+        )
+      end
     end
   end
 end
