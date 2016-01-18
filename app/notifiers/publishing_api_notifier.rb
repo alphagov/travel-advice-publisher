be991ca90419f918e8fae88c067d5559dfecec2f
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2016-01-18 10:56:45 +0000

    Update travel advice index when countries are published...
    The travel advice index page is sorted by country update timestamp and
    should therefore be updated whenever advice for a country is published.

--- app/notifiers/publishing_api_notifier.rb
+++ app/notifiers/publishing_api_notifier.rb
@@ -2,16 +2,27 @@ module PublishingApiNotifier
   class << self
     def put_content(edition)
       presenter = EditionPresenter.new(edition)
-      api = TravelAdvicePublisher.publishing_api_v2
 
       api.put_content(presenter.content_id, presenter.render_for_publishing_api)
     end
 
     def publish(edition)
       presenter = EditionPresenter.new(edition)
-      api = TravelAdvicePublisher.publishing_api_v2
 
       api.publish(presenter.content_id, presenter.update_type)
+    end
+
+    def publish_index
+      presenter = IndexPresenter.new
+
+      api.put_content(presenter.content_id, presenter.render_for_publishing_api)
+      api.publish(presenter.content_id, presenter.update_type)
+    end
+
+  private
+
+    def api
+      TravelAdvicePublisher.publishing_api_v2
     end
   end
 end
