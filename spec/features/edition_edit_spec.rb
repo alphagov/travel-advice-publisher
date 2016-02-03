4256ef18e44041707443103c59c18aa2af38dc51
Chris Patuzzo <chris@patuzzo.co.uk>
2016-02-03 10:03:31 +0000

    Remove the placeholder_ format prefix

--- spec/features/edition_edit_spec.rb
+++ spec/features/edition_edit_spec.rb
@@ -75,7 +75,7 @@ feature "Edit Edition page", :js => true do
 
       assert_publishing_api_put_content("2a3938e1-d588-45fc-8c8f-0f51814d5409", request_json_includes(
         title: "An archived title",
-        format: "placeholder_travel_advice",
+        format: "travel_advice",
         content_id: '2a3938e1-d588-45fc-8c8f-0f51814d5409',
       ))
     end
@@ -209,7 +209,7 @@ feature "Edit Edition page", :js => true do
 
     assert_publishing_api_put_content("2a3938e1-d588-45fc-8c8f-0f51814d5409", request_json_includes(
       title: "Travel advice for Albania",
-      format: "placeholder_travel_advice",
+      format: "travel_advice",
       content_id: '2a3938e1-d588-45fc-8c8f-0f51814d5409',
     ))
   end
@@ -460,7 +460,7 @@ feature "Edit Edition page", :js => true do
     expect(action.comment).to eq "Minor update"
 
     assert_publishing_api_put_content("2a3938e1-d588-45fc-8c8f-0f51814d5409", request_json_includes(
-      format: "placeholder_travel_advice",
+      format: "travel_advice",
       content_id: '2a3938e1-d588-45fc-8c8f-0f51814d5409',
     ))
 
