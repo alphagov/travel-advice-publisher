139732dcc639e7f339c5a1e0f5cb417ad493eb81
Chris Patuzzo <chris@patuzzo.co.uk>
2016-03-02 15:07:36 +0000

    Present email alert signup content items for editions

---
+++ app/presenters/email_alert_signup/edition_presenter.rb
@@ -0,0 +1,82 @@
+module EmailAlertSignup
+  class EditionPresenter
+    def initialize(edition)
+      self.edition = edition
+    end
+
+    def content_payload
+      {
+        content_id: content_id,
+        base_path: base_path,
+        format: "email_alert_signup",
+        title: edition.title,
+        description: "#{edition.title} Email Alert Signup",
+        public_updated_at: public_updated_at,
+        locale: "en",
+        publishing_app: "travel-advice-publisher",
+        rendering_app: "email-alert-frontend",
+        routes: routes,
+        details: details,
+        update_type: update_type,
+      }
+    end
+
+    def content_id
+      country.email_signup_content_id
+    end
+
+    def update_type
+      "republish"
+    end
+
+  private
+
+    attr_accessor :edition
+
+    def edition_base_path
+      "/foreign-travel-advice/#{edition.country_slug}"
+    end
+
+    def base_path
+      "#{edition_base_path}/email-signup"
+    end
+
+    def public_updated_at
+      Time.zone.now.iso8601
+    end
+
+    def routes
+      [{ path: base_path, type: "exact" }]
+    end
+
+    def details
+      {
+        signup_tags: tags,
+        summary: summary,
+        breadcrumbs: breadcrumbs,
+        govdelivery_title: edition.title,
+      }
+    end
+
+    def tags
+      { countries: [country.content_id] }
+    end
+
+    def summary
+      "You'll get an email each time #{edition.title} is updated."
+    end
+
+    def breadcrumbs
+      [
+        {
+          title: edition.title,
+          link: edition_base_path,
+        }
+      ]
+    end
+
+    def country
+      Country.find_by_slug(edition.country_slug)
+    end
+  end
+end
---
+++ spec/presenters/email_alert_signup/edition_presenter_spec.rb
@@ -0,0 +1,59 @@
+require "spec_helper"
+
+RSpec.describe EmailAlertSignup::EditionPresenter do
+  let(:edition) do
+    FactoryGirl.build(
+      :travel_advice_edition,
+      country_slug: "aruba",
+      title: "Aruba Travel Advice",
+    )
+  end
+
+  let(:email_signup_content_id) { "45b318a5-3dde-4898-b6d6-c93e65866f4e" }
+  let(:edition_content_id) { "56bae85b-a57c-4ca2-9dbd-68361a086bb3" }
+
+  around do |example|
+    Timecop.freeze { example.run }
+  end
+
+  it "validates against the email alert signup schema" do
+    presenter = described_class.new(edition)
+    expect(presenter.content_payload.as_json).to be_valid_against_schema('email_alert_signup')
+  end
+
+  it "presents the email signup content item for the edition" do
+    presenter = described_class.new(edition)
+
+    expect(presenter.content_payload).to eq({
+      content_id: email_signup_content_id,
+      base_path: "/foreign-travel-advice/aruba/email-signup",
+      title: "Aruba Travel Advice",
+      description: "Aruba Travel Advice Email Alert Signup",
+      format: "email_alert_signup",
+      locale: "en",
+      publishing_app: "travel-advice-publisher",
+      rendering_app: "email-alert-frontend",
+      public_updated_at: Time.zone.now.iso8601,
+      update_type: "republish",
+      routes: [
+        {
+          path: "/foreign-travel-advice/aruba/email-signup",
+          type: "exact",
+        }
+      ],
+      details: {
+        summary: "You'll get an email each time Aruba Travel Advice is updated.",
+        govdelivery_title: "Aruba Travel Advice",
+        signup_tags: {
+          countries: [edition_content_id],
+        },
+        breadcrumbs: [
+          {
+            title: "Aruba Travel Advice",
+            link: "/foreign-travel-advice/aruba",
+          },
+        ]
+      },
+    })
+  end
+end
