0735c3d204507a42ff7455ab0e33d8787885dc06
Graham Pengelly <graham@three-tier.com>
2016-04-25 14:46:08 +0100

    Add `ContentStoreCheck`
    This check will form part of an automated publication check too that
    confirms publication was successful. This particular check requests the
    page JSON from the content store and compares the published
    `publishing_request_id` with that stored within Travel Advice Publisher.

---
+++ spec/lib/publication_check/content_store_check_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+
+module PublicationCheck
+  describe ContentStoreCheck do
+    let(:edition){
+      FactoryGirl.create(
+        :published_travel_advice_edition,
+        country_slug: "andorra",
+        version_number: 2)
+    }
+    let(:publish_request){
+      PublishRequest.new(
+        request_id: "25107-1461581820.634-185.22.224.96-13641",
+        edition_id: edition.id
+      )
+    }
+    let(:content_store_check){ ContentStoreCheck.new }
+    let(:content_store_url){
+      "#{Plek.find('www-origin')}/api/content/foreign-travel-advice/#{edition.country_slug}"
+      # "https://www.gov.uk/api/content/foreign-travel-advice/#{edition.country_slug}"
+    }
+    let(:content_store_response){
+      double(status: 200, body: response_body)
+    }
+    let(:response_publishing_request_id){
+      "25107-1461581820.634-185.22.224.96-13641"
+    }
+    let(:response_body){
+      <<-JSON
+        {
+          "base_path": "test/base/path",
+          "content_id": "7a2554bd-9dc5-4a2e-953c-263c65ced66b",
+          "details": {
+            "publishing_request_id": "#{response_publishing_request_id}"
+          }
+        }
+      JSON
+    }
+
+    before do
+      allow(Net::HTTP).to receive(:get_response)
+        .with(URI(content_store_url))
+        .and_return(content_store_response)
+    end
+
+    it "requests the correct page" do
+      expect(Net::HTTP).to receive(:get_response).with(
+        URI(content_store_url)
+      ).and_return(content_store_response)
+      content_store_check.run(publish_request)
+    end
+
+    context "a response containing the request id" do
+      before do
+        allow(Net::HTTP).to receive(:get_response)
+          .with(URI(content_store_url))
+          .and_return(content_store_response)
+      end
+
+      it "returns true" do
+        expect(content_store_check.run(publish_request))
+          .to be(true)
+      end
+    end
+
+    context "a response containing a different request id" do
+      let(:response_publishing_request_id){
+        "25107-1461581820.634-185.22.224.96-1234"
+      }
+
+      it "returns false" do
+        expect(content_store_check.run(publish_request))
+          .to be(false)
+      end
+    end
+
+    context "the response contains no request_id" do
+      let(:response_body){
+        <<-JSON
+          {
+            "base_path": "test/base/path",
+            "content_id": "7a2554bd-9dc5-4a2e-953c-263c65ced66b",
+            "details": {}
+          }
+        JSON
+      }
+
+      it "returns false" do
+        expect(content_store_check.run(publish_request))
+          .to be(false)
+      end
+    end
+  end
+end
