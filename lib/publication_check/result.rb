3f3c227570927c9776632697d330b21a60e5286b
Graham Pengelly <graham@three-tier.com>
2016-06-24 15:51:47 +0100

    Add `report` and `failed?` methods to `Result`
    The `Result` class is returned by the check runner. This will simplify
    the rake task and allow it to return status to Jenkins.

--- lib/publication_check/result.rb
+++ lib/publication_check/result.rb
@@ -8,10 +8,37 @@ module PublicationCheck
       @publish_requests << publish_request
     end
 
-    def success?
-      #TODO check the state of the publish requests and return
-      #based on if any have failed all retries
-      true
+    def failed?
+      complete_and_unsuccessful.any?
+    end
+
+    def report
+      report_lines = publish_requests.map do |publish_request|
+        generate_report_line(publish_request)
+      end
+      report_lines.join("\n")
+    end
+
+  private
+
+    attr_reader :publish_requests
+
+    def complete_and_unsuccessful
+      publish_requests.select do |publish_request|
+        publish_request.succeeded? == false && publish_request.checks_complete?
+      end
+    end
+
+    def generate_report_line(publish_request)
+      "#{status_string(publish_request)} #{publish_request.edition_id} #{publish_request.country_slug} checked. check_count: #{publish_request.check_count}, frontend_updated: #{publish_request.frontend_updated || 'no'}"
+    end
+
+    def status_string(publish_request)
+      if publish_request.checks_complete?
+        publish_request.succeeded? ? "SUCCESS:" : "FAILURE:"
+      else
+        "ONGOING:"
+      end
     end
   end
 end
