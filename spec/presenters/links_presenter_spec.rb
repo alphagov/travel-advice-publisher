c346e1e3d45f72d46c82eb084fc9c5ae77f74592
Douglas Roper <dougdroper@gmail.com>
2016-09-12 14:36:11 +0100

    Change links presenter to send empty links hash
    Passthrough hashes have been removed from the publishing API, sending am
    empty links hash until we can standardise the breadcrumbs/sidebar
    behaviour.

--- spec/presenters/links_presenter_spec.rb
+++ spec/presenters/links_presenter_spec.rb
@@ -15,69 +15,7 @@ RSpec.describe LinksPresenter do
     let(:presented_data) { subject.present }
 
     it "returns travel advice breadcrumbs data" do
-      expect(presented_data).to eq(
-        :links => {
-          "parent" => [
-            {
-              "content_id" => "08d48cdd-6b50-43ff-a53b-beab47f4aab0",
-              "base_path" => "/foreign-travel-advice",
-              "title" => "Foreign travel advice",
-              "links" => {
-                "parent" => ["b9849cd6-61a7-42dc-8124-362d2c7d48b0"]
-              }
-            },
-            {
-              "content_id" => "b9849cd6-61a7-42dc-8124-362d2c7d48b0",
-              "base_path" => "/browse/abroad/travel-abroad",
-              "title" => "Travel abroad",
-              "links" => {
-                "parent" => ["86eb717a-fb40-42e7-83fa-d031a03880fb"]
-              }
-            },
-            {
-              "content_id" => "86eb717a-fb40-42e7-83fa-d031a03880fb",
-              "base_path" => "/browse/abroad",
-              "title" => "Passports, travel and living abroad"
-            }
-          ],
-          "related" => [
-            {
-              "content_id" => "b9849cd6-61a7-42dc-8124-362d2c7d48b0",
-              "title" => "Travel abroad",
-              "base_path" => "/browse/abroad/travel-abroad"
-            },
-            {
-              "content_id" => "95f9c380-30bc-44c7-86b4-e9c9ef0fc272",
-              "title" => "Hand luggage restrictions at UK airports",
-              "base_path" => "/hand-luggage-restrictions",
-              "links" => {
-                "parent" => ["b9849cd6-61a7-42dc-8124-362d2c7d48b0"]
-              }
-            },
-            {
-              "content_id" => "e4d06cb9-9e2e-4e82-b802-0aad013ae16c",
-              "title" => "Driving abroad",
-              "base_path" => "/driving-abroad",
-              "links" => {
-                "parent" => ["b9849cd6-61a7-42dc-8124-362d2c7d48b0"]
-              }
-            },
-            {
-              "content_id" => "86eb717a-fb40-42e7-83fa-d031a03880fb",
-              "title" => "Passports, travel and living abroad",
-              "base_path" => "/browse/abroad"
-            },
-            {
-              "content_id" => "82248bb1-c4d6-41e0-9494-d98123475626",
-              "title" => "Renew or replace your adult passport",
-              "base_path" => "/renew-adult-passport",
-              "links" => {
-                "parent" => ["86eb717a-fb40-42e7-83fa-d031a03880fb"]
-              }
-            }
-          ]
-        }
-      )
+      expect(presented_data).to eq(links: {})
     end
   end
 end
