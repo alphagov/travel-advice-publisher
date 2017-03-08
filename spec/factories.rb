3f018aaa4bb48e9c4771425ec44d95e55f80b76c
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-03-08 12:32:22 +0000

    Remove unused factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,42 +11,6 @@ FactoryGirl.define do
     end
   end
 
-  factory :disabled_user, parent: :user do
-    disabled true
-  end
-
-  factory :edition, class: TravelAdviceEdition do
-    panopticon_id 0
-    transient do
-      version_number nil
-    end
-
-    sequence(:slug) { |n| "slug-#{n}" }
-    sequence(:title) { |n| "A key answer to your question #{n}" }
-
-    after :build do |ed, evaluator|
-      if !evaluator.version_number.nil?
-        ed.version_number = evaluator.version_number
-      elsif (previous = ed.series.order(version_number: "desc").first)
-        ed.version_number = previous.version_number + 1
-      end
-    end
-
-    trait :scheduled_for_publishing do
-      state 'scheduled_for_publishing'
-      publish_at 1.day.from_now
-    end
-
-    trait :published do
-      state 'published'
-    end
-
-    trait :with_body do
-      body 'Some body text'
-    end
-
-  end
-
   factory :travel_advice_edition do
     sequence(:country_slug) {|n| "test-country-#{n}" }
     sequence(:title) {|n| "Test Country #{n}" }
@@ -56,6 +20,7 @@ FactoryGirl.define do
   # These factories only work when used with FactoryGirl.create
   factory :draft_travel_advice_edition, :parent => :travel_advice_edition do
   end
+
   factory :published_travel_advice_edition, :parent => :travel_advice_edition do
     after :create do |tae|
       tae.published_at ||= Time.zone.now.utc
@@ -63,6 +28,7 @@ FactoryGirl.define do
       tae.save!
     end
   end
+
   factory :archived_travel_advice_edition, :parent => :travel_advice_edition do
     after :create do |tae|
       tae.state = 'archived'
