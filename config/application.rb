701a2c3563ec2f5e16e099c3e00e859677169742
Daniel Roseman <daniel.roseman@digital.cabinet-office.gov.uk>
2017-03-13 15:35:00 +0000

    Update configuration files for Rails upgrade.
    This was done by running `rake rails:update` and manually reviewing
    each suggested change.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 # Pick the frameworks you want:
 # require "active_record/railtie"
@@ -35,18 +35,6 @@ module TravelAdvicePublisher
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-    config.i18n.enforce_available_locales = true
-
-    # Enable escaping HTML in JSON.
-    config.active_support.escape_html_entities_in_json = true
 
     # Disable Rack::Cache
     config.action_dispatch.rack_cache = nil
