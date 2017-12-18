8014691b5a9f414b950fea220cdd81d59ee53e95
Graham Pengelly <graham@three-tier.com>
2017-12-18 14:17:13 +0000

    Add email-alert-api bearer token
    Email-alert-api is getting authentication/authorisation. This commit
    adds a bearer token for it.

--- config/initializers/email_alert_api.rb
+++ config/initializers/email_alert_api.rb
@@ -2,5 +2,6 @@ require "gds_api/email_alert_api"
 require "plek"
 
 TravelAdvicePublisher.email_alert_api = GdsApi::EmailAlertApi.new(
-  Plek.current.find("email-alert-api")
+  Plek.current.find("email-alert-api"),
+  bearer_token: ENV.fetch("EMAIL_ALERT_API_BEARER_TOKEN", "bearer_token")
 )
