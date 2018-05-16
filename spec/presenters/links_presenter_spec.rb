fa31a1f599f1f972d8e2cde5e4da4e45bdb975de
chao-xian <gds@kelvingan.co.uk>
2018-05-16 15:39:31 +0100

    Add primary org to travel advice pages

--- spec/presenters/links_presenter_spec.rb
+++ spec/presenters/links_presenter_spec.rb
@@ -14,10 +14,11 @@ RSpec.describe LinksPresenter do
   describe "present" do
     let(:presented_data) { subject.present }
 
-    it "returns travel advice breadcrumbs data" do
+    it "returns travel advice links data" do
       expect(presented_data).to eq(links: {
         parent: ["08d48cdd-6b50-43ff-a53b-beab47f4aab0"],
-        meets_user_needs: ["5118d7b4-215d-45e6-bd20-15d7bc21314f"]
+        meets_user_needs: ["5118d7b4-215d-45e6-bd20-15d7bc21314f"],
+        primary_publishing_organisation: ["9adfc4ed-9f6c-4976-a6d8-18d34356367c"],
       })
     end
   end
