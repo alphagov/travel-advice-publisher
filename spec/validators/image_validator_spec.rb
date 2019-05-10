740b7f115089eb36ba72be229cf6eac0ce2a918b
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-05-10 09:35:02 +0100

    Add an image validator
    This checks that the image can be opened by ImageMagick.

---
+++ spec/validators/image_validator_spec.rb
@@ -0,0 +1,55 @@
+RSpec.describe ImageValidator do
+  class DocumentWithImage
+    include Mongoid::Document
+
+    field "image", type: File
+
+    validates :image, image: true
+  end
+
+  let(:image) { nil }
+  let(:document) { DocumentWithImage.new(image: image) }
+
+  shared_examples "is valid" do
+    it "should be valid" do
+      expect(document).to be_valid
+      expect(document.errors).to be_empty
+    end
+  end
+
+  shared_examples "is invalid" do
+    it "should be invalid" do
+      expect(document).to_not be_valid
+      expect(document.errors[:image]).to_not be_empty
+    end
+  end
+
+  context "given no image" do
+    include_examples "is valid"
+  end
+
+  context "given an HTML file" do
+    let(:image) { file_fixture("example.html").open }
+    include_examples "is invalid"
+  end
+
+  context "given a PNG file" do
+    let(:image) { file_fixture("example.png").open }
+    include_examples "is valid"
+  end
+
+  context "given an HTML file with a PNG extension" do
+    let(:image) { file_fixture("example.html.png").open }
+    include_examples "is invalid"
+  end
+
+  context "given a JPEG file" do
+    let(:image) { file_fixture("example.jpg").open }
+    include_examples "is valid"
+  end
+
+  context "given a JPEG with a PNG extension" do
+    let(:image) { file_fixture("example.jpg.png").open }
+    include_examples "is invalid"
+  end
+end
