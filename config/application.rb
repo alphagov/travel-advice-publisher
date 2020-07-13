0e77797f57474cb08d1152a03bb0aae27cd6d2fc
Andrew White <andrew.white@unboxed.co>
2020-07-13 17:32:05 +0100

    Move constants into an initializer
    This helps when upgrading the standard Rails config files.

--- config/application.rb
+++ config/application.rb
@@ -13,20 +13,6 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module TravelAdvicePublisher
-  # Maslow need ID for Travel Advice Publisher
-  NEED_CONTENT_ID = "5118d7b4-215d-45e6-bd20-15d7bc21314f".freeze
-
-  INDEX_CONTENT_ID = "08d48cdd-6b50-43ff-a53b-beab47f4aab0".freeze
-  # The parent page is /browse/abroad/travel-abroad
-  INDEX_PARENT_CONTENT_ID = "b9849cd6-61a7-42dc-8124-362d2c7d48b0".freeze
-  INDEX_EMAIL_SIGNUP_CONTENT_ID = "1aebfc97-7723-4cb6-82f4-434639efc185".freeze
-
-  COUNTRY_FORMAT = "travel_advice".freeze
-
-  INDEX_FORMAT = "travel_advice_index".freeze
-
-  PRIMARY_ORG_CONTENT_ID = "9adfc4ed-9f6c-4976-a6d8-18d34356367c".freeze
-
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
