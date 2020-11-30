87dc15891ac14ef5906fa7e64821255add713b87
Kevin Dew <kevindew@me.com>
2020-11-30 21:28:23 +0000

    Remove unnecessary rake rspec task
    RSpec will install this task manually.

--- app/assets/javascripts/admin/editions/form.js
+++ app/assets/javascripts/admin/editions/form.js
@@ -1,7 +1,7 @@
 window.AdminEditionsForm = {
   showChangeNotesIfMajorChange: function showChangeNotesIfMajorChange () {
     var $form = $('.js-edition-form')
-    var $fieldset = $('.js-change-notes', this.$form)
+    var $fieldset = $('.js-change-notes', $form)
     var $radioButtons = $('input[type=radio]', $fieldset)
     var $majorChangeRadioButton = $('.js-edition-update-major', $fieldset)
     var $changeNotesSection = $('.js-change-notes-section', $fieldset)
