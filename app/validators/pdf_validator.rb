2d09ce96c893a5bb87a7d3021dc101cb0749f0ed
Rebecca Pearce <rebecca.pearce@digital.cabinet-office.gov.uk>
2021-07-09 17:50:15 +0100

    Fix deprecation message
    Fix the message: `DEPRECATION WARNING: Calling `<<` to an 
    ActiveModel::Errors message array in order to add an error is 
    deprecated. Please call `ActiveModel::Errors#add` instead.`

--- app/validators/pdf_validator.rb
+++ app/validators/pdf_validator.rb
@@ -5,9 +5,9 @@ class PdfValidator < ActiveModel::EachValidator
     PDF::Reader.new(value.path)
 
     unless value.path.downcase.match?(/\.pdf$/)
-      record.errors[attribute] << "is a PDF, but has the extension '#{File.extname(value.path)}'"
+      record.errors.add(attribute, message: "is a PDF, but has the extension '#{File.extname(value.path)}'")
     end
   rescue PDF::Reader::MalformedPDFError
-    record.errors[attribute] << "is not a PDF"
+    record.errors.add(attribute, message: "is not a PDF")
   end
 end
