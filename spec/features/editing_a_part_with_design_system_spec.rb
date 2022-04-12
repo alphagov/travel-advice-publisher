ac7390443cf8bc7cafb70e671c76a6dc55f04a7b
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-04-12 15:53:33 +0100

    Update edit part feature specs to ensure archived and published parts cannot be edited
    Parts belonging to published and archived editions should not be able to
    be edited. These tests ensure that if they try and manually hit the edit
    endpoint, they are redirected to the review action.

--- spec/features/editing_a_part_with_design_system_spec.rb
+++ spec/features/editing_a_part_with_design_system_spec.rb
@@ -3,7 +3,7 @@ feature "Editing a part" do
     before do
       login_as_stub_user_with_design_system_permission
       @edition = create(
-        :travel_advice_edition_with_parts,
+        :draft_travel_advice_edition,
         country_slug: "aruba",
         summary: "Advice summary",
         version_number: 1,
@@ -26,7 +26,7 @@ feature "Editing a part" do
       expect(find("#part_slug").value).to eq @part.slug
     end
 
-    scenario "Parts are prefilled when you visit the edit page" do
+    scenario "User edits a part" do
       visit edit_admin_edition_path(@edition)
       click_on "Change #{@part.title}"
       fill_in "Title", with: "New title"
@@ -36,6 +36,52 @@ feature "Editing a part" do
       within "#parts" do
         expect(all(".govuk-summary-list__row")[0]).to have_content "New title"
       end
+    end
+  end
+
+  context "Published edition" do
+    before do
+      login_as_stub_user_with_design_system_permission
+      @edition = create(
+        :published_travel_advice_edition,
+        country_slug: "aruba",
+        summary: "Advice summary",
+        version_number: 1,
+      )
+      @part = @edition.parts.create!(
+        title: "Some Part Title!",
+        body: "This is some **version** text.",
+        slug: "part-one",
+        order: 1,
+      )
+    end
+
+    scenario "User manually visits the edit path" do
+      visit edit_admin_country_edition_part_path(@edition.country_slug, @edition, @part)
+      expect(page).to have_current_path(review_admin_country_edition_part_path(@edition.country_slug, @edition, @part))
+    end
+  end
+
+  context "Archived edition" do
+    before do
+      login_as_stub_user_with_design_system_permission
+      @edition = create(
+        :archived_travel_advice_edition,
+        country_slug: "aruba",
+        summary: "Advice summary",
+        version_number: 1,
+      )
+      @part = @edition.parts.create!(
+        title: "Some Part Title!",
+        body: "This is some **version** text.",
+        slug: "part-one",
+        order: 1,
+      )
+    end
+
+    scenario "User visits page and sees summary list table" do
+      visit edit_admin_country_edition_part_path(@edition.country_slug, @edition, @part)
+      expect(page).to have_current_path(review_admin_country_edition_part_path(@edition.country_slug, @edition, @part))
     end
   end
 end
