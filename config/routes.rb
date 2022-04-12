afe8b55a3d82493c0f5f24985cc3019d005014b1
David Gisbey <david.gisbey@digital.cabinet-office.gov.uk>
2022-04-12 15:56:53 +0100

    Add the review endpoint
    This commit adds the review endpoint and view. The review page is
    just a summary list component with the parts attrs.
    
    It also deals with adding the relevant redirects to ensure parts belonging
    to published and archived editions are not editable and parts belonging
    to draft editions are not reviewable (you're redirected to the edit page).

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :countries, only: %i[index show] do
       resources :editions, only: [:create] do
-        resources :parts, only: %i[new create edit update]
+        resources :parts, only: %i[new create edit update show] do
+          get "review", on: :member
+        end
       end
     end
 
