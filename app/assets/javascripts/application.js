db7522d6c83ee10d468654a17e954f6d9e9c8f59
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2022-04-14 00:51:33 +0100

    Add tracking back in for Travel advice publisher
    Add Google analytics tracking back into the application layout.
    
    During the migration we lost the automatic analytics tracking, this needs to be added back in.
    
    Add this to the layout so that everything is tracked automatically.
    
    https://trello.com/c/YlaNbIF6/373-add-tracking-back-in-for-travel-advice-publisher

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,4 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/all_components
+//= require govuk_publishing_components/analytics
+//= require analytics
