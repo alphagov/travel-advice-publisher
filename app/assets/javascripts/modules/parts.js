015c4397093ff60b7b18416f7e3c63a3c13f4af8
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2022-05-05 07:50:02 +0100

    fix linting issues

--- app/assets/javascripts/modules/parts.js
+++ app/assets/javascripts/modules/parts.js
@@ -16,62 +16,62 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
   }
 
   Parts.prototype.initSortable = function () {
-    if (!this.module.querySelector("#parts")) return
+    if (!this.module.querySelector('#parts')) return
 
-    window.Sortable.create(this.module.querySelector("#parts"), {
-      handle: ".part__drag-handle",
-      chosenClass: "part__drag-item--chosen",
-      onSort: function() {
+    window.Sortable.create(this.module.querySelector('#parts'), {
+      handle: '.part__drag-handle',
+      chosenClass: 'part__drag-item--chosen',
+      onSort: function () {
         this.updateOrderIndexes()
       }.bind(this)
     })
   }
 
-  Parts.prototype.initAddNewPartBtn = function() {
-    var btn = this.module.querySelector(".js-add-new-part")
+  Parts.prototype.initAddNewPartBtn = function () {
+    var btn = this.module.querySelector('.js-add-new-part')
 
-    btn.addEventListener('click', function(e) {
+    btn.addEventListener('click', function (e) {
       e.preventDefault()
 
       var template = document.getElementById('new-part-template')
       template = template.content.cloneNode(true)
       template = this.initialiseTemplate(template)
 
-      this.module.querySelector("#parts").append(template)
+      this.module.querySelector('#parts').append(template)
       this.updateOrderIndexes()
     }.bind(this))
   }
 
-  Parts.prototype.initialiseTemplate = function( template ) {
+  Parts.prototype.initialiseTemplate = function (template) {
     var newId = this.module.querySelectorAll('#parts .govuk-accordion__section').length + 1
     template.firstElementChild.innerHTML = template.firstElementChild.innerHTML.replaceAll('{{ insert-part-index-here }}', newId)
-    template.querySelector(".govuk-accordion__section-content").classList.add("part__show-by-default")
-    template.querySelector(".edition-part__id").remove()
+    template.querySelector('.govuk-accordion__section-content').classList.add('part__show-by-default')
+    template.querySelector('.edition-part__id').remove()
 
-    this.initTitleListener(template.querySelector(".js-part__title-input"))
-    this.initRemovePartBtn(template.querySelector(".js-part__remove-button"))
+    this.initTitleListener(template.querySelector('.js-part__title-input'))
+    this.initRemovePartBtn(template.querySelector('.js-part__remove-button'))
 
     return template
   }
 
-  Parts.prototype.updateOrderIndexes = function() {
-    var orderInputs = this.module.querySelectorAll(".edition-part__order")
+  Parts.prototype.updateOrderIndexes = function () {
+    var orderInputs = this.module.querySelectorAll('.edition-part__order')
 
     for (var i = 0; i < orderInputs.length; i++) {
-      orderInputs[i].setAttribute("value", i + 1)
+      orderInputs[i].setAttribute('value', i + 1)
     }
   }
 
-  Parts.prototype.initRemovePartBtns = function() {
-    var buttons = this.module.querySelectorAll(".js-part__remove-button")
+  Parts.prototype.initRemovePartBtns = function () {
+    var buttons = this.module.querySelectorAll('.js-part__remove-button')
 
     for (var i = 0; i < buttons.length; i++) {
-      this.initRemovePartBtn( buttons[i] )
+      this.initRemovePartBtn(buttons[i])
     }
   }
 
-  Parts.prototype.initRemovePartBtn = function( button ) {
-    button.addEventListener('click', function(e) {
+  Parts.prototype.initRemovePartBtn = function (button) {
+    button.addEventListener('click', function (e) {
       e.preventDefault()
 
       var partIndex = e.currentTarget.getAttribute('data-part-index')
@@ -82,28 +82,28 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
     }.bind(this))
   }
 
-  Parts.prototype.initTitleListeners = function() {
-    var titleInputs = this.module.querySelectorAll(".js-part__title-input")
+  Parts.prototype.initTitleListeners = function () {
+    var titleInputs = this.module.querySelectorAll('.js-part__title-input')
 
     for (var i = 0; i < titleInputs.length; i++) {
       var titleInput = titleInputs[i]
-      this.initTitleListener( titleInput )
+      this.initTitleListener(titleInput)
     }
   }
 
-  Parts.prototype.initTitleListener = function( titleInput ) {
-    titleInput.addEventListener('change', function(e) {
+  Parts.prototype.initTitleListener = function (titleInput) {
+    titleInput.addEventListener('change', function (e) {
       var target = e.currentTarget
-      var partIndex = target.getAttribute("data-part-index")
-      var title = this.module.querySelector("#part-title-" + partIndex)
-      var slugInput = this.module.querySelector("input[name='edition[parts_attributes][" + partIndex + "][slug]']")
+      var partIndex = target.getAttribute('data-part-index')
+      var title = this.module.querySelector('#part-title-' + partIndex)
+      var slugInput = this.module.querySelector('input[name="edition[parts_attributes][' + partIndex + '][slug]"]')
 
       title.innerText = target.value
-      slugInput.value = this.convertTitleToSlug( target.value )
+      slugInput.value = this.convertTitleToSlug(target.value)
     }.bind(this))
   }
 
-  Parts.prototype.convertTitleToSlug = function ( title ) {
+  Parts.prototype.convertTitleToSlug = function (title) {
     return title.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-')
   }
 
