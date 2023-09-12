00fe6ba3a3a8f8d6eed0217cad46f09433d1a1fb
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2023-09-12 11:46:32 +0100

    Remove afghanistan topical event hack

--- app/models/afghanistan_links_presenter.rb
+++
@@ -1,9 +0,0 @@
-module AfghanistanLinksPresenter
-  def afganistan_topical_event_payload
-    { topical_events: [afganistan_topical_event_content_id] }
-  end
-
-  def afganistan_topical_event_content_id
-    "39084ee0-b77e-456b-93cb-f95a56f74b50"
-  end
-end
