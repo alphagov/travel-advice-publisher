96bfc234db84d60aba42bc009c72ffa7bb50eb98
Minno Dang <mndang@thoughtworks.com>
2024-03-26 19:12:53 +0000

    Refactoring removing dead code
    Since the validation checks for scheduled_publication_time always expects a value and will error when there are none, there's no need to check for the publication time presence anymore.

--- app/controllers/admin/schedulings_controller.rb
+++ app/controllers/admin/schedulings_controller.rb
@@ -1,18 +1,15 @@
 class Admin::SchedulingsController < ApplicationController
   before_action :skip_slimmer
-  before_action :load_country_and_edition, only: %i[new create destroy]
+  before_action :load_country_and_edition
   before_action :redirect_unless_can_schedule
 
   def new; end
 
   def create
     begin
-      if scheduled_publication_time.present?
-        @edition.scheduled_publication_time = scheduled_publication_time
-
-        if @edition.save && @edition.schedule_for_publication(current_user)
-          redirect_to admin_country_path(@country.slug), notice: "#{@country.name} travel advice is scheduled to publish on #{@edition.scheduled_publication_time.strftime('%B %d, %Y %H:%M %Z')}." and return
-        end
+      @edition.scheduled_publication_time = scheduled_publication_time
+      if @edition.save && @edition.schedule_for_publication(current_user)
+        redirect_to admin_country_path(@country.slug), notice: "#{@country.name} travel advice is scheduled to publish on #{@edition.scheduled_publication_time.strftime('%B %d, %Y %H:%M %Z')}." and return
       end
     rescue StandardError => e
       @edition.errors.add(:scheduled_publication_time, e)
@@ -33,7 +30,7 @@ class Admin::SchedulingsController < ApplicationController
 private
 
   def redirect_unless_can_schedule
-    redirect_to admin_country_path(@country.slug) and return unless can_schedule_edition?
+    redirect_to admin_country_path(@country.slug) unless can_schedule_edition?
   end
 
   def scheduled_publication_time
