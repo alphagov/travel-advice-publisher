c5b8b5745fe8c02e882e3950c27527731ebc3482
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-07-30 17:41:56 +0100

    Fix breaking changes
    - universal analytics has been removed and no longer works, removing related files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,5 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/all_components
-//= require govuk_publishing_components/analytics
-//= require analytics
 //= require ./modules/parts
 //= require ./modules/broken_links
 //= require ./modules/paste_html_to_govspeak
