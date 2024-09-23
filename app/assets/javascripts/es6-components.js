7d2141cab7b1947c21764df3fc4261908dfce58c
Alex Newton <alex.newton@digital.cabinet-office.gov.uk>
2024-09-23 10:19:17 +0100

    Upgrade to GOV.UK Frontend V5

--- app/assets/javascripts/es6-components.js
+++ app/assets/javascripts/es6-components.js
@@ -10,9 +10,6 @@
 //= require govuk_publishing_components/components/accordion
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/checkboxes
-//= require govuk_publishing_components/components/character-count
-//= require govuk_publishing_components/components/error-summary
 //= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/skip-link
-//= require govuk_publishing_components/components/tabs
