096c500122d9070fb236375f2ebe433c5e12f3aa
Ryan Brown <ryan.brown@digital.cabinet-office.gov.uk>
2025-01-27 11:21:17 +0000

    Upgrade to Rails 7.2
    This upgrade includes a number of files which are used when making a Rails app available as a Progressive Web App.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ Bundler.require(*Rails.groups)
 module TravelAdvicePublisher
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     config.active_support.cache_format_version = 7.1
 
