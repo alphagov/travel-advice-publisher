de774e44d7797a5757278f018aa647929d78f1cb
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2026-01-26 15:40:54 +0000

    Remove image card JS reference
    - image card component JS was removed a few versions ago

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require govuk_publishing_components/dependencies
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/govspeak
-//= require govuk_publishing_components/components/image-card
 //= require govuk_publishing_components/components/metadata
 //= require govuk_publishing_components/components/service-navigation
 //= require govuk_publishing_components/components/table
